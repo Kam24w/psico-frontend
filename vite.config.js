2384a5945a43f8dabafc813e93c74fe061583e57
Lhansqw <lhanmls37437@gmail.com>
2026-05-22 00:55:40 -0500

    añadir notificaciones visuales y optimizar el tiempo de carga de las pantallas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('face-api.js')) {
+              return 'vendor-face-api'
+            }
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor-react'
+            }
+            return 'vendor-core'
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
   server: {
     port: 5173,
   }
